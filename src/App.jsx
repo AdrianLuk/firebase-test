cabb3911689a932d69a5c0c36652d03428cfd782
Adrian <lukabaseball@hotmail.com>
2022-03-23 17:14:53 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyACxPIuAevcoRf7R067pPua2nKxlcyouIM',
+  authDomain: 'fir-test-1392f.firebaseapp.com',
+  projectId: 'fir-test-1392f',
+  storageBucket: 'fir-test-1392f.appspot.com',
+  messagingSenderId: '487829068402',
+  appId: '1:487829068402:web:5eea3ff53e7984bd52c0eb',
+  measurementId: 'G-22YK339VT9',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type='button' onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className='App-link'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
