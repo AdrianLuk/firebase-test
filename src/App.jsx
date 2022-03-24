be94992d866e8f201b5a70cf97251ddd0478da49
Adrian <lukabaseball@hotmail.com>
2022-03-24 12:44:17 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Vite + React! yoooooooo</p>
+        <p>Hello Vite + React! yoooooooo dudeeee</p>
         <p>
           <button type='button' onClick={() => setCount((count) => count + 1)}>
             count is: {count}
